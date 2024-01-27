c2e4ab7784884087f641ab2c10e517093940b47c
LilPiere <thomcat115@gmail.com>
2024-01-27 20:50:35 +0000

    npc moves

--- npcManager.gd
+++ npcManager.gd
@@ -6,12 +6,17 @@ const npc = preload("res://scenes/player.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var new_npc = npc.instantiate()
-	new_npc.setShirtColour(Color(randi() % 255,randi() % 255,randi() % 255,1))
+	new_npc.setShirtColour(Color(randi() % 127,randi() % 127,randi() % 127,0))
 	add_child(new_npc)
+	position = Vector3 (15,0,randi() % 4 + 1)
 	
 	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	position.x -= delta 
+	
+
+func spawnNPC():
 	pass
