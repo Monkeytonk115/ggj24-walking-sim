40616bc965c160c15631c6a0049b77bb34d18496
Matthew <monkeytonk115@gmail.com>
2024-01-27 09:03:17 +0000

    fix notes positioning

--- interface/notes_slider.gd
+++ interface/notes_slider.gd
@@ -26,15 +26,15 @@ func add_new_button(pos : int) -> void:
 	var new_button = ButtonIndicator.instantiate()
 	new_button.position.x = 1900
 	if pos == 1:
-		new_button.position.y = 50
+		new_button.position.y = 25
 		new_button.texture = button_1
 	if pos == 2:
-		new_button.position.y = 120
+		new_button.position.y = 85
 		new_button.texture = button_2
 	if pos == 3:
-		new_button.position.y = 190
+		new_button.position.y = 150
 		new_button.texture = button_3
 	if pos == 4:
-		new_button.position.y = 250
+		new_button.position.y = 220
 		new_button.texture = button_4
 	$ButtonHolder.add_child(new_button)
