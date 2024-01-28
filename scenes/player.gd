dd4414393ee2c24af4da24db0fd1332c4adb327f
LilPiere <thomcat115@gmail.com>
2024-01-28 11:13:43 +0000

    npc head rotate

--- scenes/player.gd
+++ scenes/player.gd
@@ -20,6 +20,7 @@ func _process(delta):
 		if destination != position:
 			position.x -= 0.03
 			($head as MeshInstance3D).rotate_y(1 * delta)
+			($head as MeshInstance3D).look_at(Vector3(0,0,0),Vector3(0,1,0))
 		else:
 			queue_free()
 
