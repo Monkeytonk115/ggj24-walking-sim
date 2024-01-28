97ca3edac219c0128046cb58de5eed13660e5e14
Matthew <monkeytonk115@gmail.com>
2024-01-28 14:57:11 +0000

    change instrument playing

--- scenes/player.gd
+++ scenes/player.gd
@@ -12,8 +12,6 @@ var foot1 = preload("res://audio/foots-001.ogg")
 @export var legs : String
 @export var shoe : String
 
-var instrument_model
-
 
 func _ready():
 	audio = get_node("body/AudioStreamPlayer3D")
@@ -44,22 +42,6 @@ func npcMove():
 	$AnimationPlayer.speed_scale = 4 * moveMultiplier
 
 
-# Animate holstering instrument
-func holster_instrument():
-	if instrument_model:
-		instrument_model.queue_free()
-
-
-# Animate drawing instrument
-func draw_instrument(instrument : Instrument):
-	instrument_model = instrument.model.instantiate()
-	$hand_right.add_child(instrument_model)
-
-
-# Play the instrument sequence at the bpm
-func play_instrument():
-	#instrument_model.find_child("Animation").play("play")
-	pass
 
 func step():
 	#print("step")
