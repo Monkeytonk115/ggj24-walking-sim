99e9a53f00ac0523c086fb4cb4aa2ac7d2276cfd
Matthew <monkeytonk115@gmail.com>
2024-01-28 11:04:21 +0000

    tidy up npcmanager

--- npcManager.gd
+++ npcManager.gd
@@ -1,21 +1,10 @@
 extends Node3D
 
 const npc = preload("res://scenes/player.tscn")
-var i = 0
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	position 
-	
 	$Timer.start()
-	
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	#position.x -= delta 
-	pass
-	
 
 
 func _on_timer_timeout():
